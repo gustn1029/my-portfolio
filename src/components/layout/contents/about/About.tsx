9773f32c465da7a2c80f03f19e974625f0d4731f
gustn1029 <gustn1029@gmail.com>
2024-04-18 23:44:15 +0900

    about update

--- src/components/layout/contents/about/About.tsx
+++ src/components/layout/contents/about/About.tsx
@@ -7,13 +7,13 @@ const About = () => {
   return (
     <section className="pt-[10%]">
       <article className="flex justify-between">
+        <figure className="p-[20px] bg-white rounded-lg shadow-md">
+          <Image src={MyProfile} alt="my-image" />
+        </figure>
         <div>
           <SectionTItle text="About Me" />
           text
         </div>
-        <figure className="p-[20px] bg-white rounded-lg shadow-md">
-          <Image src={MyProfile} alt="my-image" />
-        </figure>
       </article>
     </section>
   );
