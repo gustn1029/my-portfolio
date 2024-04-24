59d970039a6cc9fdfeda64dcac3c37eb945f5171
gustn1029 <gustn1029@gmail.com>
2024-04-24 18:16:59 +0900

    tailwindcss screens update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,13 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      'tablet': '600px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+      'mobile' : {'max': '820px'},
+      'phone' : {"max": "471px"}
+    }
   },
   plugins: [],
 };
