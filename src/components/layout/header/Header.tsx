798e462b00a8dbdd3f89496abfc531573c8bffdb
hs0820 <59814042+gustn1029@users.noreply.github.com>
2024-04-29 16:32:03 +0900

    헤더 로그아웃 버튼, url 변경

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,14 +3,21 @@
 import Navigation from "@/components/navigation/Navigation";
 import useScrollPosition from "@/hook/useScrollPosition";
 import React, { useEffect } from "react";
+import { PiSignOutBold } from "react-icons/pi";
+import { signOut, useSession } from "next-auth/react";
 
 const Header = () => {
   const scrollPosition = useScrollPosition();
+  const { data: session } = useSession();
 
   return (
     <header
       className={`fixed flex top-0 left-[50%] translate-x-[-50%] h-[50px] items-center justify-between common-px 
-      ${scrollPosition === 0 ? " w-full translate-y-0" : " w-[90%] bg-white bg-opacity-60 translate-y-5 rounded-[30px] shadow-lg"}
+      ${
+        scrollPosition === 0
+          ? " w-full translate-y-0"
+          : " w-[90%] bg-white bg-opacity-60 translate-y-5 rounded-[30px] shadow-lg"
+      }
       transition-all
       backdrop-blur-sm
       z-[100]
@@ -20,8 +27,13 @@ const Header = () => {
       phone:gap-y-[10px]
       `}
     >
-      <h2 className="dancing-font font-bold text-[20px] phone:text-[16px]">KimHyeonSoo</h2>
-      <Navigation />
+      <h2 className="dancing-font font-bold text-[20px] phone:text-[16px]">
+        KimHyeonSoo
+      </h2>
+      <div className="flex gap-x-[20px] items-end">
+        <Navigation />
+        {session?.user && <PiSignOutBold className="mb-[3px] text-gray-500 hover:text-black cursor-pointer transition-all" onClick={() => signOut()} />}
+      </div>
     </header>
   );
 };
