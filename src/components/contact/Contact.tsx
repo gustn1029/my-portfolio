d8c092197948c3b744a661e263db6d07f9520a16
hs0820 <59814042+gustn1029@users.noreply.github.com>
2024-04-29 16:23:29 +0900

    contact 컴포넌트 설정

---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <li className='section-bg'>
+        
+    </li>
+  )
+}
+
+export default Contact
---
+++ src/components/contact/ContactList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ContactList = () => {
+  return (
+    <div>
+        <ul>
+            
+        </ul>
+    </div>
+  )
+}
+
+export default ContactList
