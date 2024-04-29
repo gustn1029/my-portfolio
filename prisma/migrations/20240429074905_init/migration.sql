522191c30ad859b4c3da8ba235e2036da8b21531
hs0820 <59814042+gustn1029@users.noreply.github.com>
2024-04-29 17:21:25 +0900

    prisma, db 재설정

--- prisma/migrations/20240429074905_init/migration.sql
+++ prisma/migrations/20240429074905_init/migration.sql
@@ -10,8 +10,8 @@ CREATE TABLE "User" (
 CREATE TABLE "Post" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "content" TEXT,
-    "published" BOOLEAN NOT NULL DEFAULT false,
     "authorId" INTEGER NOT NULL,
     CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
