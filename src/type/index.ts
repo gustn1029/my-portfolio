6520ec05453b6127d367183f3253874e8fbc77da
hs0820 <59814042+gustn1029@users.noreply.github.com>
2024-04-29 16:40:37 +0900

    타입 추가

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,27 +1,44 @@
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
 import { ReactNode } from "react";
+import { FieldError, FieldErrors, UseFormRegisterReturn } from "react-hook-form";
 
 export interface ChildrenProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export interface NavigationOption {
-    text: string;
-    url: string;
+  text: string;
+  url: string;
 }
 
 export interface ProjectProps {
-    title: string;
-    desc: string;
-    skill: string;
-    url: string;
-    imageUrl: StaticImport | string;
-    period: string;
-    isFile: boolean;
-  }
+  title: string;
+  desc: string;
+  skill: string;
+  url: string;
+  imageUrl: StaticImport | string;
+  period: string;
+  isFile: boolean;
+}
 
-  export interface Post {
-    title: string;
-    email: string;
-    content: string;
-  }
+export interface InputProps {
+  type?: "text" | "number" | "password" | "email";
+  label: string;
+  placeholder?: string;
+  register: UseFormRegisterReturn<string>;
+  children?: ReactNode;
+  error?: FieldErrors<any>;
+  isLabelTextHidden?: boolean;
+  description?: string;
+  errorView?: FieldError;
+  disabled?: boolean;
+  isRequired?: boolean;
+  inputClassName?: string;
+  labelClassName?: string;
+}
+
+export interface Post {
+  title: string;
+  email: string;
+  content: string;
+}
