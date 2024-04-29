d179a183926cc0c56e8d1a2cab4b0a8a72cf9afd
hs0820 <59814042+gustn1029@users.noreply.github.com>
2024-04-29 16:33:24 +0900

    navigation 애니메이션 방식 수정

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { NavigationOption } from "@/type";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavItem from "./NavItem";
 
 const navList: NavigationOption[] = [
@@ -9,11 +9,33 @@ const navList: NavigationOption[] = [
   { text: "About", url: "/#about" },
   { text: "Skill", url: "/#skill" },
   { text: "Project", url: "/project" },
-  { text: "Contact", url: "#contact" },
+  { text: "Contact", url: "/contact" },
 ];
 
 const Navigation = () => {
-  const [selected, setSelected] = useState(0);
+  const [selected, setSelected] = useState<number>(() => {
+    const currentPath = window.location.pathname;
+    const foundIndex = navList.findIndex((el) => el.url.includes(currentPath));
+    return foundIndex !== -1 ? foundIndex : 0;
+  });
+
+  useEffect(() => {
+    const pathChange = () => {
+      const currentPath = window.location.pathname;
+      const foundIndex = navList.findIndex((el) =>
+        el.url.includes(currentPath)
+      );
+      setSelected(foundIndex !== -1 ? foundIndex : 0);
+    };
+
+    pathChange();
+
+    window.addEventListener("load", pathChange);
+
+    return () => {
+      window.removeEventListener("load", pathChange);
+    };
+  }, []);
 
   return (
     <ul className="flex gap-x-[20px] items-center">
