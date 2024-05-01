0795ea80c834fa7b7238382f91dac32f8231cc12
hs0820 <59814042+gustn1029@users.noreply.github.com>
2024-05-02 00:03:28 +0900

    컨텍트 데이터 확인 리스트 컴포넌트 추가

--- src/components/contact/ContactList.tsx
+++ src/components/contact/ContactList.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import useSWR from "swr";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { Post } from "@/type";
+import Contact from "./Contact";
 
 const ContactList = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const [dataList, setDataList] = useState<Post[]>([]);
+
+  if (status !== "authenticated") {
+    router.push("/signin");
+    return <></>;
+  }
+
+  const fetcher = (url: string) => fetch(url).then((r) => r.json());
+  const { data, error, isLoading } = useSWR(`/api/post/1`, fetcher);
+
+  useEffect(() => {
+    setDataList([...data]);
+  }, [data]);
+
   return (
     <div>
-        <ul>
-            
-        </ul>
+      <ul className="grid gap-y-[50px]">
+        {dataList.map((el, idx) => (
+          <Contact
+            key={`${el.title}_${idx}`}
+            title={el.title}
+            email={el.email}
+            content={el.content}
+          />
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ContactList
+export default ContactList;
