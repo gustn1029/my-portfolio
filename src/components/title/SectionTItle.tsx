fed60060fbc63e92e2cede3b61ae3b0cf6333308
hs0820 <59814042+gustn1029@users.noreply.github.com>
2024-05-02 19:11:17 +0900

    sectionTitle compoenet 반응형 작업

--- src/components/title/SectionTItle.tsx
+++ src/components/title/SectionTItle.tsx
@@ -9,7 +9,7 @@ const SectionTItle = ({text, desc = ""}:TitleProps) => {
   return (
     <div className='pb-[40px]'>
         <h3 className='font-bold text-[4rem] laptop:text-[3rem] phone:text-[2.5rem] leading-[1] pb-[10px] mobile:pb-[5px]'>{text}</h3>
-        <p className='text-[1.3rem] mobile:text-[1rem] text-gray-500'>{desc}</p>
+        <p className='text-[1.3rem] mobile:text-[0.9rem] text-gray-500'>{desc}</p>
     </div>
   )
 }
